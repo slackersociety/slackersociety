6e817c736f107252c5843240bdf0a0c5b8c97f60
slackersociety <omarchoudhury2@gmail.com>
2020-05-10 05:06:20 -0400

    Update index-page.js

--- src/templates/index-page.js
+++ src/templates/index-page.js
@@ -3,6 +3,7 @@ import PropTypes from 'prop-types'
 import { Link, graphql } from 'gatsby'
 
 import Layout from '../components/Layout'
+import Features from '../components/Features'
 import BlogRoll from '../components/BlogRoll'
 
 export const IndexPageTemplate = ({
